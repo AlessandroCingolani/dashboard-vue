46b567e32f7ed2d6dbdd188e5e81230977725204
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-14 15:10:46 +0100

    [add]:donuts graphics

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,31 @@
 <script>
 import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement,ArcElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, Legend, ArcElement, BarElement, CategoryScale, LinearScale)
 
 export default {
   name: 'MainComponent',
-  components: { Bar },
+  components: { Bar,Doughnut },
   data() {
     return {
       chartData: {
-        labels: [ 'January', 'February', 'March'],
-        datasets: [ { data: [40, 20, 12] } ]
+        labels: [ '0-16', '17-25', '26-35','36-45','46-60'],
+        datasets: [ { data: [100, 20, 12] } ]
       },
-      chartOptions: {
-        maintainAspectRatio: false,
-        responsive: true
+      dataDonut : {
+        labels: ['Android', 'iOS', 'Windows', 'Linux','macOS'],
+        datasets: [
+        {
+        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+        data: [40, 20, 80, 10]
+        } 
+        ]
+      },
+      options :{
+        responsive: true,
+        maintainAspectRatio: false
       }
     }
   }
@@ -26,13 +36,32 @@ export default {
   <main>
     <div class="main_container">
       <div class="first">
-        <Bar
-          id="my-chart-id"
-          :options="chartOptions"
-          :data="chartData"
-        />
-
       </div>
+        <div class="bar">
+          <div class="card h-100 text-center">
+            <div class="card-header">
+              Operating System
+            </div>
+            <div class="card-body">
+              <Bar
+                id="my-chart-id"
+                :options="options"
+                :data="chartData"
+              />
+            </div>
+          </div>
+       
+        </div>
+        <div class="donut">
+          <div class="card h-100 text-center">
+            <div class="card-header h-25">
+              Operating System
+            </div>
+            <div class="card-body h-75">
+              <Doughnut :data="dataDonut" :options="options" />
+            </div>
+          </div>
+        </div>
     </div>
   </main>
 </template>
@@ -42,11 +71,20 @@ export default {
 <style lang="scss" scoped>
   .main_container {
     display: grid;
-    grid-template-columns: repeat(2,1fr);
+    padding: 20px;
+    grid-template-columns: 1fr 1fr;
     grid-template-rows: 350px 350px 400px 350px;
+    gap: 20px;
     .first {
-        padding: 20px;
-        grid-column: 1/ 3;
-      }
+      grid-column: 1/ 3;
+    }
+    .bar {
+      grid-column: 1;
+      grid-row: 2;
+    }
+    .donut {
+      grid-column: 2;
+      grid-row: 2;
+    }
   }
 </style>
